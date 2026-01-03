cc625423da815441eb068cb3a350b1e7cc2f118e
pertech <pertech.org>
2026-01-03 12:00:10 -0600

    added circleshape and player.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame 
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,19 +12,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
-    dt = 0
-    
-    running = True
-    while running:
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
 
         log_state()
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-    pygame.quit()
     
 if __name__ == "__main__":
     main()
