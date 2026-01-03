ea4cd78a92f8a98ab8051d76232d32b40c229fa9
pertech <pertech.org>
2026-01-03 00:58:13 -0600

    fps functionality added

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    while screen:
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
         log_state()
         screen.fill(color="black")
         pygame.display.flip()
-        pass
-
-
+        dt = clock.tick(60) / 1000
+    pygame.quit()
+    
 if __name__ == "__main__":
     main()
