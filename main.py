2378e19112dd650c21cbe0c4838ac6f784b248b9
pertech <pertech.org>
2026-01-04 10:22:01 -0600

    added rotation method to player

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    dt = 0
     while True:
         log_state()
         
@@ -22,11 +23,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        log_state()
+        dt = clock.tick(60) / 1000
+        player.update(dt)
+        
         screen.fill(color="black")
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
     
 if __name__ == "__main__":
     main()
