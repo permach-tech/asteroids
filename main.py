71c69676ba9900de77a19aaf1dc5981461e8d5e3
pertech <pertech.org>
2026-01-28 22:10:14 -0600

    added collisions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import pygame 
+import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -15,24 +18,34 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
-
     dt = 0
-    
 
     while True:
         log_state()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         dt = clock.tick(60) / 1000
         updatable.update(dt)
-        
+
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+
         screen.fill(color="black")
         for sprite in drawable:
             sprite.draw(screen)
--- player.py
+++ player.py
@@ -37,4 +37,4 @@ class Player(CircleShape):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
-        self.position += rotated_with_speed_vector
+        self.position += rotated_with_speed_vector        
